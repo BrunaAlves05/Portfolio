f2c0f91c423a34b60b11d9a3f111e4b491ab5a47
Bruna Santos <100356855+BrunaAlves05@users.noreply.github.com>
2022-12-11 17:35:14 -0300

    Add files via upload

---
+++ portfólio/index.js
@@ -0,0 +1,16 @@
+
+// passar dos slides
+var count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function() {
+    nextImage();
+}, 1500)
+
+function nextImage(){
+    count++;
+    if(count>2) {
+        count = 1;
+    }
+    document.getElementById("radio"+count).checked = true;
+}
